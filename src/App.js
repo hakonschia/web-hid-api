df533b2fd672d6d89a357ad902517c85e904b71e
Håkon Schia <hakonschia5@gmail.com>
2020-02-22 17:53:58 +0100

    Add error message if WebHID is not available

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Home from './components/Home.react';
 function App() {
     return (
         <div className="App">
-            <Home></Home>
+            {
+                navigator.hid ?
+                    <Home></Home>
+                    :
+                    <div>
+                        <h3>The WebHID API is not available in your browser.</h3>
+                        <h4>If you are using a Chromium based browser, make sure you are on the latest version and have <a href="chrome://flags/#enable-experimental-web-platform-features">Experimental features enabled</a></h4>
+                    </div>
+            }
         </div>
     );
 }
